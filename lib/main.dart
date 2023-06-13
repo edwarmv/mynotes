dc73bdcc5ff8a62f9bf8d248b8bc38448f31fbe0
Edwar Martinez Vale <edwar_kenedy@hotmail.com>
2023-06-12 23:00:50 -0400

    feat: create home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:mynotes/firebase_options.dart';
 import 'package:mynotes/views/login_view.dart';
 import 'package:mynotes/views/register_view.dart';
 
@@ -10,6 +13,34 @@ void main() {
       colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
       useMaterial3: true,
     ),
-    home: const LoginView(),
+    home: const HomePage(),
   ));
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home')),
+      body: FutureBuilder(
+          future: Firebase.initializeApp(
+              options: DefaultFirebaseOptions.currentPlatform),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.done:
+                final user = FirebaseAuth.instance.currentUser;
+                if (user?.emailVerified ?? false) {
+                  print('You are a verified user');
+                } else {
+                  print('You need to verify your email first');
+                }
+                return const Text('Done');
+              default:
+                return const Text('Loading...');
+            }
+          }),
+    );
+  }
 }
