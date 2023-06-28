7d9ada820aaf1768fee7ac2aea567dec844d6552
Edwar Martinez Vale <edwar_kenedy@hotmail.com>
2023-06-27 23:25:14 -0400

    feat: step-13

--- test/auth_test.dart
+++ test/auth_test.dart
@@ -122,7 +122,10 @@ class MockAuthProvider implements AuthProvider {
     if (!isInitialized) throw NotInitializedException();
     if (email == 'foo@bar.com') throw UserNotFoundAuthExceptions();
     if (password == 'foobar') throw WrongPasswordAuthExceptions();
-    const user = AuthUser(isEmailVerified: false);
+    const user = AuthUser(
+      isEmailVerified: false,
+      email: 'foo@bar.com',
+    );
     _user = user;
     return Future.value(user);
   }
@@ -140,7 +143,10 @@ class MockAuthProvider implements AuthProvider {
     if (!isInitialized) throw NotInitializedException();
     final user = _user;
     if (user == null) throw UserNotFoundAuthExceptions();
-    const newUser = AuthUser(isEmailVerified: true);
+    const newUser = AuthUser(
+      isEmailVerified: true,
+      email: 'foo@bar.com',
+    );
     _user = newUser;
   }
 }
