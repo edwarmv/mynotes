292e4a01a0c157cc3b2061890617190621e8f32d
Edwar Martinez Vale <edwar_kenedy@hotmail.com>
2023-07-01 09:47:27 -0400

    feat: step-16

--- lib/views/notes/new_note_view.dart
+++ lib/views/notes/new_note_view.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/services/auth/auth_service.dart';
 import 'package:mynotes/services/curd/notes_services.dart';
